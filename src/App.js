320baa6efcd3995f36fdeda632521c7646093c29
Caleb Cochrane <Calebcochranebil33@gmail.com>
2023-06-02 00:54:37 -0700

    scatterchart

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import { WarningAlert } from "./Alert";
 import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
 import "./nprogress.css";
 import WelcomeScreen from "./WelcomeScreen";
+import {
+    ScatterChart,
+    Scatter,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+} from "recharts";
 
 class App extends Component {
     state = {
@@ -15,6 +24,19 @@ class App extends Component {
         seletedLocation: "all",
         numberOfEvents: 32,
         showWelcomeScreen: undefined,
+    };
+
+    // API data for charts
+    getData = () => {
+        const { locations, events } = this.state;
+        const data = locations.map((location) => {
+            const number = events.filter(
+                (event) => event.location === location
+            ).length;
+            const city = location.split(", ").shift();
+            return { city, number };
+        });
+        return data;
     };
 
     async componentDidMount() {
@@ -32,7 +54,7 @@ class App extends Component {
                     this.setState({
                         events: events.slice(0, this.state.numberOfEvents),
                         locations: extractLocations(events),
-                    }); //this.setState({ events, locations: extractLocations(events) });
+                    });
                 }
             });
         }
@@ -90,16 +112,35 @@ class App extends Component {
 
         return (
             <div className="App">
-                <h1>Developer Meet Ups</h1>
-                <CitySearch
-                    locations={this.state.locations}
-                    updateEvents={this.updateEvents}
-                />
-                <NumberOfEvents
-                    numberOfEvents={this.state.numberOfEvents}
-                    updateEvents={this.updateEvents}
-                />
-                <WarningAlert text={offlineMessage}></WarningAlert>
+                <div>
+                    <h1>Developer Meet Ups</h1>
+                    <CitySearch
+                        locations={this.state.locations}
+                        updateEvents={this.updateEvents}
+                    />
+                    <NumberOfEvents
+                        numberOfEvents={this.state.numberOfEvents}
+                        updateEvents={this.updateEvents}
+                    />
+                    <WarningAlert text={offlineMessage}></WarningAlert>
+                </div>
+                <h4>Events in each city</h4>
+                <ResponsiveContainer height={400}>
+                    <ScatterChart
+                        margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+                    >
+                        <CartesianGrid />
+                        <XAxis type="category" dataKey="city" name="city" />
+                        <YAxis
+                            type="number"
+                            dataKey="number"
+                            name="number of events"
+                            allowDecimals={false}
+                        />
+                        <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+                        <Scatter data={this.getData()} fill="#8884d8" />
+                    </ScatterChart>
+                </ResponsiveContainer>
                 <EventList events={this.state.events} />
                 <WelcomeScreen
                     showWelcomeScreen={this.state.showWelcomeScreen}
